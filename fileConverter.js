c1ab4769e6b4013cbab9d5fe596a4e4bac3856be
sathish <98166189+sathishsam14@users.noreply.github.com>
2023-07-14 17:01:05 +0530

    Add files via upload

---
+++ fileConverter.js
@@ -0,0 +1,74 @@
+let oneEle = document.getElementById("one");
+let twoEle = document.getElementById("two");
+let threeEle = document.getElementById("three");
+let fourEle = document.getElementById("four");
+let fiveEle = document.getElementById("five");
+
+let oneElem = document.getElementById("ones");
+let twoElem = document.getElementById("twos");
+let threeElem = document.getElementById("threes");
+let fourElem = document.getElementById("fours");
+let fiveElem = document.getElementById("fives");
+
+let headELe = document.getElementById("heading");
+
+function audio() {
+    headELe.textContent = "Audio Converter";
+    oneEle.textContent = "MP4";
+    twoEle.textContent = "MP3";
+    threeEle.textContent = "WAV";
+    fourEle.textContent = "AAC";
+    fiveEle.textContent = "M4A";
+
+    oneElem.textContent = "MP4";
+    twoElem.textContent = "MP3";
+    threeElem.textContent = "WAV";
+    fourElem.textContent = "AAC";
+    fiveElem.textContent = "M4A";
+
+}
+
+function video() {
+    headELe.textContent = "Video Converter";
+    oneEle.textContent = "AVI";
+    twoEle.textContent = "MP4";
+    threeEle.textContent = "FLV ";
+    fourEle.textContent = "WMV";
+    fiveEle.textContent = "MOV";
+
+    oneElem.textContent = "AVI";
+    twoElem.textContent = "MP4";
+    threeElem.textContent = "FLV ";
+    fourElem.textContent = "WMV";
+    fiveElem.textContent = "MOV";
+}
+
+
+function image() {
+    headELe.textContent = "Image Converter";
+    oneEle.textContent = "JPEG";
+    twoEle.textContent = "PNG";
+    threeEle.textContent = "GIF";
+
+    oneElem.textContent = "JPEG";
+    twoElem.textContent = "PNG";
+    threeElem.textContent = "GIF";
+
+
+}
+
+function Document() {
+    headELe.textContent = "Document Converter";
+    oneEle.textContent = "DOCX";
+    twoEle.textContent = "PDF";
+    threeEle.textContent = "TXT";
+    fourEle.textContent = "WPS";
+    fiveEle.textContent = "DOC";
+
+    oneElem.textContent = "DOCX";
+    twoElem.textContent = "PDF";
+    threeElem.textContent = "TXT";
+    fourElem.textContent = "WPS";
+    fiveElem.textContent = "DOC";
+
+}
